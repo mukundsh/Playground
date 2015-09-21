c631e7e24b126ec878436b63fdd24fb545ab3c6e
unknown <mukundsh@beast-pc.fareast.corp.microsoft.com>
2015-09-20 20:33:01 -0700

    print all anagrams of a string

--- Playground2/StringProblems.cs
+++ Playground2/StringProblems.cs
@@ -8,6 +8,70 @@ namespace Playground2
 {
     public static class StringProblems
     {
+        public static IEnumerable<string> GetAllAnagramsOfString(string input)
+        {
+            char[] inputChars = input.ToCharArray();
+            Array.Sort(inputChars);
+            input = new string(inputChars);
+            Console.WriteLine(input);
+            yield return input;
+            while (true)
+            {
+                input = GetNextPermutation(inputChars);
+                if (input == null)
+                {
+                    yield break;
+                }
+
+                yield return input;
+            }
+        }
+
+        // http://stackoverflow.com/questions/1622532/algorithm-to-find-next-greater-permutation-of-a-given-string
+        private static string GetNextPermutation(char[] input)
+        {
+            int i = input.Length - 1;
+            while(i > 0 && input[i] <= input[i - 1])
+            {
+                i--;
+            }
+
+            if (i == 0)
+            {
+                return null;
+            }
+
+            int j = input.Length - 1;
+            while(input[j] <= input[i -1])
+            {
+                j--;
+            }
+
+            Swap(input, i -1, j);
+            
+            j = input.Length - 1;
+            var temp = input[i - 1];
+            while (i < j)
+            {
+                temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+                i++;
+                j--;
+            }
+
+            string nextPermutation = new string(input);
+            Console.WriteLine(nextPermutation);
+            return nextPermutation;
+        }
+
+        private static void Swap(char[] input, int i, int j)
+        {
+            char temp = input[i];
+            input[i] = input[j];
+            input[j] = temp;
+        }
+
         public static void Quicksort(IComparable[] arr)
         {
             Quicksort(arr, 0, arr.Length - 1);
---
+++ PlaygroundTests/StringProblemsTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Playground2;
+
+namespace PlaygroundTests
+{
+    [TestClass]
+    public class StringProblemsTests
+    {
+        [TestMethod]
+        public void TestGetAllAnagrams()
+        {
+            string input = "abcde";
+            string[] actual = StringProblems.GetAllAnagramsOfString(input).ToArray();
+
+            Assert.AreEqual(actual.Length, 120);
+        }
+    }
+}
